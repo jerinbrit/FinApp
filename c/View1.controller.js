f1cb88a7f1890c93d64c115d9d12f733d2aaaacb
britmanjerin <britmanjer@hotmail.com>
2023-08-25 16:59:12 +0400

    Update View1.controller.js

--- c/View1.controller.js
+++ c/View1.controller.js
@@ -40,7 +40,7 @@ sap.ui.define([
 			var j = $.Deferred();
 			$.ajax({
 				type: 'GET',
-				url: '/britmanjerin/FinApp/main/m/cust.json',
+				url: 'https://api.github.com/repos/britmanjerin/tst/contents/new.json',
 				success: function(data) {
 					data = data ? JSON.parse(data) : [];
 					that.oModel.setData(data);
