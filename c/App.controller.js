146a93a65a529c4369e6d1313cca93dd22ba44e5
britmanjerin <britmanjer@hotmail.com>
2023-08-25 12:49:41 +0400

    Update App.controller.js

--- c/App.controller.js
+++ c/App.controller.js
@@ -1,10 +1,10 @@
 sap.ui.define([
-		"FabFinV3/BaseController",
+		"FabFinV3/controller/BaseController",
 		"sap/ui/model/json/JSONModel"
 	], function (BaseController, JSONModel) {
 		"use strict";
 
-		return BaseController.extend("FabFinV3.App", {});
+		return BaseController.extend("FabFinV3.controller.App", {});
 
 	}
 );
