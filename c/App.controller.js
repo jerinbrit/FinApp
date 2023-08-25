a0f9615d4f799b8d19708db3787a6b675c5b84b1
britmanjerin <britmanjer@hotmail.com>
2023-08-25 12:35:53 +0400

    Create App.controller.js

---
+++ c/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+		"FabFinV3/BaseController",
+		"sap/ui/model/json/JSONModel"
+	], function (BaseController, JSONModel) {
+		"use strict";
+
+		return BaseController.extend("FabFinV3.App", {});
+
+	}
+);
